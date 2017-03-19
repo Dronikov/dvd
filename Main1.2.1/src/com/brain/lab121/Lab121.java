0d2561c7e7c8207dd44bc77c3f28f0b2b579533b
Viacheslav Dronikov <dvd@te.net.ua>
2017-03-20 01:40:22 +0200

    Laba 1.2.1

---
+++ Main1.2.1/src/com/brain/lab121/Lab121.java
@@ -0,0 +1,11 @@
+package com.brain.lab121;
+public class Lab121 {
+    public static void main(String[] args) {
+        System.out.println("  * * *      *    *     *     * ");
+        System.out.println("      *     * *   *     *    * *");
+        System.out.println("      *    *   *   *   *    *   *");
+        System.out.println("*     *   * * * *   * *    * * * *");
+        System.out.println(" * * *    *     *    *     *     * ");
+    }
+}
+
