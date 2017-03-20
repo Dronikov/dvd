c4028b96aacc0277a44e0ddca9d40108395cfe7a
Viacheslav Dronikov <dvd@te.net.ua>
2017-03-20 03:08:34 +0200

    Laba 1.4.5

---
+++ Main1.4.5/src/com/brain/lab145/Lab145.java
@@ -0,0 +1,9 @@
+package com.brain.lab145;
+public class Lab145 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 16;
+        System.out.println("8<<4=" + (b<<a));
+        System.out.println("8>>4=" + (b>>a));
+    }
+}
