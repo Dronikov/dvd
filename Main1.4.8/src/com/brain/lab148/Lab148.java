ae0bdcda0dcf86993d9fb56838c623d6abad0bb5
Viacheslav Dronikov <dvd@te.net.ua>
2017-03-20 13:22:18 +0200

    Laba 1.4.8

---
+++ Main1.4.8/src/com/brain/lab148/Lab148.java
@@ -0,0 +1,29 @@
+package com.brain.lab148;
+public class Lab148 {
+    public static void main(String[] args) {
+    int a = 5;
+    float b = 3.1456926f;
+       System.out.println("a+b=(int)="+(int)(a+b));
+       System.out.println("a+b=(float)="+(a+b));
+       }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
