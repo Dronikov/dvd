6dc102fad4a55153ec980ec7b62f2e59e99aa2b4
Viacheslav Dronikov <dvd@te.net.ua>
2017-03-20 13:13:40 +0200

    Laba 1.4.6

---
+++ Main1.4.6/src/com/brain/lab146/Lab146.java
@@ -0,0 +1,11 @@
+package com.brain.lab146;
+public class Lab146 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 16;
+        System.out.println("8&10=" + (b&a));
+        System.out.println("8|10=" + (b|a));
+        System.out.println("8^10=" + (b^a));
+        System.out.println("~10=" + (~a));
+    }
+}
