864947f0e8a1cde21acd3fad83155a1f2df08541
Viacheslav Dronikov <dvd@te.net.ua>
2017-03-20 02:12:11 +0200

    Laba 1.4.1

---
+++ Main1.4.1/src/com/brain/lab141/Lab141.java
@@ -0,0 +1,22 @@
+package com.brain.lab141;
+
+public class Lab141 {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 4;
+        float e = 3.14f;
+        double f = 3.14;
+        char g = 'a';
+        boolean h = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
