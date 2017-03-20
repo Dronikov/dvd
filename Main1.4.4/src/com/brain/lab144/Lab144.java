287cbb72d8c0b6976b058b6d7cdcebac276cc97a
Viacheslav Dronikov <dvd@te.net.ua>
2017-03-20 02:35:14 +0200

    Laba 1.4.4

---
+++ Main1.4.4/src/com/brain/lab144/Lab144.java
@@ -0,0 +1,13 @@
+package com.brain.lab144;
+
+public class Lab144 {
+    public static void main(String[] args) {
+        int a = 1;
+        for (int i = 0; i < a; i++) {
+            System.out.println("inc i++ =" + i++);
+            System.out.println("dec i-- =" + i--);
+            System.out.println("inc ++i =" + ++i);
+            System.out.println("dec --i =" + --i);
+        }
+    }
+}
