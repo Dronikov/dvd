22a4647a94575045bc2afa7c4dc77a8f65a912a4
Viacheslav Dronikov <dvd@te.net.ua>
2017-03-20 02:14:07 +0200

    Laba 1.4.2

---
+++ Main1.4.2/src/com/brain/lab142/Lab142.java
@@ -0,0 +1,12 @@
+package com.brain.lab142;
+public class Lab142 {
+    public static void main(String[] args) {
+        int a = 50;
+        int b  = 6;
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+        System.out.println("a/b="+(a/b));
+        System.out.println("a*b="+(a*b));
+        System.out.println("a%b="+(a%b));
+    }
+}
